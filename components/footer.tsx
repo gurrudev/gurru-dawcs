7fd2b8f796c73806919e10fcf4379593da1608a5
gurrudev <pawarash000@outlook.com>
2024-11-08 15:08:23 +0530

    UPDATE: Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,27 +6,27 @@ import Image from "next/image";
 
 export function Footer() {
   return (
-    <footer className="border-t w-full h-16">
-      <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
-        <div className="flex items-center gap-3">
-          <Image src={logoG} alt="logo" width={30} height={30} />
-          <p className="text-center">
-            Build by{" "}
-            <Link
-              className="px-1 text-orange-300"
-              href="https://ashutoshpawar.live"
-            >
-              ASHUTOSH
-            </Link>
-            . &copy; 2024 GurruDawcs.
-          </p>
-        </div>
+      <footer className="border-t w-full h-16">
+          <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
+              <div className="flex items-center gap-3">
+                  <Image src={logoG} alt="logo" width={30} height={30} />
+                  <p className="text-center">
+                      Build by{" "}
+                      <Link
+                          className="px-1 lowercase font-semibold hover:underline"
+                          href="https://ashutoshpawar.live"
+                      >
+                          ĀŚUTOṢA
+                      </Link>
+                      . &copy; 2024 GurruDawcs.
+                  </p>
+              </div>
 
-        <div className="gap-4 items-center hidden md:flex">
-          <FooterButtons />
-        </div>
-      </div>
-    </footer>
+              <div className="gap-4 items-center hidden md:flex">
+                  <FooterButtons />
+              </div>
+          </div>
+      </footer>
   );
 }
 
